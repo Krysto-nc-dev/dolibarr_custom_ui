c3b3ec76cce85ba18ac57700b6407579d9a62b68
Velten Stoyann <krysto.dev@gmail.com>
2024-07-08 23:19:59 +1100

    add download documents query et download product documents

--- frontend/src/screens/private/UserProductDetailsScreen.jsx
+++ frontend/src/screens/private/UserProductDetailsScreen.jsx
@@ -4,21 +4,52 @@ import { useGetProductDetailsQuery } from '../../slices/dolibarr/dolliProductApi
 import { useGetStockmovementsQuery } from '../../slices/dolibarr/dolliStockmovementApiSlice';
 import Loader from '../../components/shared/Loader';
 import Barcode from 'react-barcode';
+import { useGetDocumentsQuery } from '../../slices/dolibarr/dolliDocumentApiSlice';
+import { DOLIBAR_URL } from '../../constants';
+import { DOLIBARR_API_KEY } from '../../slices/constants';
 
 const UserProductDetailsScreen = () => {
   const { id: productId } = useParams();
   const { data: product, isLoading: loadingProduct, error: errorProduct } = useGetProductDetailsQuery(productId);
   const { data: stockmovements, isLoading: loadingStock, error: errorStock } = useGetStockmovementsQuery();
+  const { data: documents, isLoading: loadingDocuments, error: errorDocuments } = useGetDocumentsQuery({
+    modulepart: 'product',
+    id: productId,
+  });
 
-  if (loadingProduct || loadingStock) {
+
+  const handleDownload = async (modulepart, file) => {
+    const url = `${DOLIBAR_URL}/documents/download?modulepart=${modulepart}&original_file=${encodeURIComponent(file)}`;
+    
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'DOLAPIKEY': DOLIBARR_API_KEY,
+        },
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      const linkSource = `data:${data['content-type']};base64,${data.content}`;
+      const downloadLink = document.createElement('a');
+      downloadLink.href = linkSource;
+      downloadLink.download = data.filename;
+      downloadLink.click();
+    } catch (error) {
+      console.error('Failed to download document:', error);
+    }
+  };
+  if (loadingProduct || loadingStock || loadingDocuments) {
     return <Loader />;
   }
 
-  if (errorProduct || errorStock) {
+  if (errorProduct || errorStock || errorDocuments) {
     return (
       <p className="text-red-500">
-        {typeof (errorProduct || errorStock).data.message === 'string'
-          ? (errorProduct || errorStock).data.message
+        {typeof (errorProduct || errorStock || errorDocuments).data.message === 'string'
+          ? (errorProduct || errorStock || errorDocuments).data.message
           : 'Une erreur est survenue'}
       </p>
     );
@@ -27,12 +58,27 @@ const UserProductDetailsScreen = () => {
   const stockValue = product.stock_reel * parseFloat(product.price);
   const filteredStockMovements = stockmovements.filter((movement) => movement.product_id === productId);
 
+  const formatSize = (size) => {
+    const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+    let i = 0;
+    while (size >= 1024 && i < units.length - 1) {
+      size /= 1024;
+      i++;
+    }
+    return `${Math.round(size * 10) / 10} ${units[i]}`;
+  };
+
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md">
       <div className="flex justify-between items-start mb-6">
         <div>
           <h1 className="text-xl font-bold">{product.label}</h1>
-          <p className='m-2'><strong>Valeur du stock:</strong> <span className='text-white font-bold bg-secondaryColor py-1 px-3 rounded-full'>{stockValue.toLocaleString()} XPF </span></p>
+          <p className='m-2'>
+            <strong>Valeur du stock:</strong>
+            <span className='text-white font-bold bg-secondaryColor py-1 px-3 rounded-full'>
+              {stockValue.toLocaleString()} XPF
+            </span>
+          </p>
         </div>
         <Barcode value={product.barcode} />
       </div>
@@ -50,7 +96,7 @@ const UserProductDetailsScreen = () => {
         <div className="bg-gray-100 p-4 rounded-lg">
           <h2 className="text-xl font-semibold mb-2">Informations supplémentaires</h2>
           <p>
-            <strong>En vente:</strong> 
+            <strong>En vente:</strong>
             <span className={product.status_buy === "0" ? 'text-green-700' : 'text-red-700'}>
               {product.status_buy === "0" ? ' Oui' : ' Non'}
             </span>
@@ -62,6 +108,52 @@ const UserProductDetailsScreen = () => {
           <p><strong>Pays:</strong> {product.country_code}</p>
         </div>
       </div>
+
+      {/* Section des documents */}
+      <div className="mt-6">
+        <h2 className="text-xl font-bold mb-4">Documents</h2>
+        <table className="min-w-full bg-white">
+          <thead className="bg-primaryColor">
+            <tr>
+              <th className="py-2 px-4 text-left text-xs font-semibold text-white uppercase tracking-wider">Nom</th>
+              <th className="py-2 px-4 text-left text-xs font-semibold text-white uppercase tracking-wider">Taille</th>
+              <th className="py-2 px-4 text-left text-xs font-semibold text-white uppercase tracking-wider">Date</th>
+              <th className="py-2 px-4 text-left text-xs font-semibold text-white uppercase tracking-wider">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {documents && documents.ecmfiles_infos && documents.ecmfiles_infos.length > 0 ? (
+              documents.ecmfiles_infos.map((docInfo) => {
+                const document = documents[parseInt(docInfo.position) - 1];
+                if (!document) {
+                  return null;
+                }
+                return (
+                  <tr key={document.name}>
+                    <td className="py-2 px-4 border-b border-gray-200">{document.name}</td>
+                    <td className="py-2 px-4 border-b border-gray-200">{formatSize(document.size)}</td>
+                    <td className="py-2 px-4 border-b border-gray-200">{new Date(document.date * 1000).toLocaleDateString()}</td>
+                    <td className="py-2 px-4 border-b border-gray-200">
+                      <button
+                        onClick={() => handleDownload('product', `${document.level1name}/${document.name}`)}
+                        className="text-primaryColor underline"
+                      >
+                        Télécharger
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan="4" className='text-dangerColor font-bold text-center py-2'>Aucun document disponible.</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+
+      {/* Section des mouvements de stock */}
       <div className="mt-6">
         <h2 className="text-xl font-bold mb-4">Mouvements de Stock</h2>
         <table className="min-w-full bg-white">
@@ -74,8 +166,8 @@ const UserProductDetailsScreen = () => {
           </thead>
           <tbody>
             {filteredStockMovements.map((movement) => (
-              <tr 
-                key={movement.id} 
+              <tr
+                key={movement.id}
                 className={movement.type === "0" ? 'bg-green-200' : 'bg-red-200'}
               >
                 <td className="py-2 px-4 border-b border-gray-200">{new Date(movement.datem * 1000).toLocaleDateString()}</td>
